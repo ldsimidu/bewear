649c824d75234e1500c5b08b5406570dc7dcbfd6
Lucas Simidu <ldsimidu@gmail.com>
2026-03-02 17:36:14 -0300

    feat: home main banners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,27 @@ export default function Home() {
   return (
     <>
       <Header />
+      <div className="p-5">
+        <Image
+          src="/banners/banner-01.png"
+          alt="Banner, Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
+
+      <div className="p-5">
+        <Image
+          src="/banners/banner-02.png"
+          alt="Banner, Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 }
--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -21,7 +21,12 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between p-5">
       <Link href="/">
-        <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
+        <Image
+          src="/bewear-logo/logo.svg"
+          alt="BEWEAR"
+          width={100}
+          height={26.14}
+        />
       </Link>
 
       <div className="flex items-center">
