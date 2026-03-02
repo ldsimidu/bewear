2596266eff8f58092763022b33b52f599391deae
Lucas Simidu <ldsimidu@gmail.com>
2026-03-02 00:55:55 -0300

    feat: add authenticated user state to header menu

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import LogInForm from "./components/log-in-form";
@@ -5,29 +6,32 @@ import SignUpForm from "./components/sign-up-form";
 
 const Authentication = () => {
   return (
-    <div className="flex h-screen w-screen justify-center bg-slate-100 p-6">
-      <Tabs
-        defaultValue="sign-in"
-        className="mt-5 mb-5 w-[500px] rounded-md bg-slate-200 p-6"
-      >
-        <TabsList className="relative flex w-full bg-slate-300 pt-6 pb-6">
-          <TabsTrigger className="pt-6 pb-6" value="sign-in">
-            Sign In
-          </TabsTrigger>
-          <TabsTrigger className="pt-6 pb-6" value="sign-up">
-            Create account
-          </TabsTrigger>
-        </TabsList>
+    <>
+      <Header />
+      <div className="flex h-screen w-screen justify-center bg-slate-100 p-6">
+        <Tabs
+          defaultValue="sign-in"
+          className="mt-5 mb-5 w-[500px] rounded-md bg-slate-200 p-6"
+        >
+          <TabsList className="relative flex w-full bg-slate-300 pt-6 pb-6">
+            <TabsTrigger className="pt-6 pb-6" value="sign-in">
+              Sign In
+            </TabsTrigger>
+            <TabsTrigger className="pt-6 pb-6" value="sign-up">
+              Create account
+            </TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="sign-in">
-          <LogInForm />
-        </TabsContent>
+          <TabsContent value="sign-in">
+            <LogInForm />
+          </TabsContent>
 
-        <TabsContent value="sign-up">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+          <TabsContent value="sign-up">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { icons, MenuIcon } from "lucide-react";
+import { icons, LogInIcon, LogOutIcon, MenuIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
+import { authClient } from "@/lib/auth-client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import {
   Sheet,
@@ -13,9 +17,12 @@ import {
 } from "../ui/sheet";
 
 const Header = () => {
+  const { data: session } = authClient.useSession();
   return (
     <header className="flex items-center justify-between p-5">
-      <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
+      <Link href="/">
+        <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
+      </Link>
 
       <div className="flex items-center">
         <Sheet>
@@ -28,6 +35,57 @@ const Header = () => {
             <SheetHeader>
               <SheetTitle>Menu</SheetTitle>
             </SheetHeader>
+            {session?.user ? (
+              <>
+                <div className="flex justify-between space-y-6 p-5">
+                  <div className="flex items-center gap-3">
+                    <Avatar>
+                      <AvatarImage
+                        src={session?.user?.image as string | undefined}
+                      />
+                      <AvatarFallback>
+                        {session?.user?.name?.split(" ")?.[0]?.[0]}
+                        {session?.user?.name?.split(" ")?.[1]?.[0]}
+                      </AvatarFallback>
+                    </Avatar>
+
+                    <div>
+                      <h3 className="font-semibold">{session?.user?.name}</h3>
+                      <span className="text-muted-foreground block text-xs">
+                        {session?.user?.email}
+                      </span>
+                    </div>
+                  </div>
+
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={() => authClient.signOut()}
+                  >
+                    <LogOutIcon />
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <div className="flex items-center">
+                <div className="items-center p-5">
+                  <h2 className="font-semibold">
+                    You are not logged into any account... :(
+                  </h2>
+                  <h3 className="font-extralight">
+                    If you want the complete experience of BEWEAR, please create
+                    your account or log in.
+                  </h3>
+                </div>
+                <div className="p-5">
+                  <Button size="icon" asChild variant="outline">
+                    <Link href="/authentication">
+                      <LogInIcon />
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            )}
           </SheetContent>
         </Sheet>
       </div>
